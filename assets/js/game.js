3fa901f14001c71c617b0c0a2d8d6dcc05f87d30
unknown <ivory62280@gmail.com>
2023-09-08 09:14:56 -0700

    added console log and introduced playerName variable, cleaned up code for readability

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playername 
+console.log(playerName);
+
+function fight() {
+   window.alert("The fight has begun!");
+};
+fight();
+//fight();
