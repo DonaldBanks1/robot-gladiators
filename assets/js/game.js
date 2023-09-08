3f583911fb36413600728a7cb4fdbc4e20d52685
unknown <ivory62280@gmail.com>
2023-09-08 11:36:32 -0700

    added console log variable for name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,19 @@
 var playerName = window.prompt("What is your robot's name?");
 //Note the lack of quotation marks around playername 
+
 console.log(playerName);
+//tells console to log the players name
+
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+//We wanted to take the name of our robot and place it into a sentence that would read, "Our robot's name is ." To do this, we needed to combine a string with a variable. This is called string concatenation.
 
 function fight() {
    window.alert("The fight has begun!");
 };
 fight();
 //fight();
+
